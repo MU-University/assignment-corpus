834c84d85ada760f1da0c84cc482ca1e304242aa
asldev <eshonkulov.asliddin.mars@gmail.com>
2025-12-14 13:13:17 +0500

    Fix

---
+++ submission/student2-corpus/eshonkulov-asliddin/src/main/java/com/example/SimpleApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gradle!");
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
